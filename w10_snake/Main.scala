e3cb74ef89366823e7c2e6a17f7ddbfc717f2d49
villemovander <ville.movander@gmail.com>
2025-11-25 14:07:49 +0100

    hek

--- w10_snake/Main.scala
+++ w10_snake/Main.scala
@@ -1,5 +1,5 @@
 package snake
-///////////////////////////
+/////////////////////////// banan
 def createOnePlayerGame(): Unit = 
   Settings.default.windowTitle = "Snake: One Player"
   OnePlayerGame().play()
